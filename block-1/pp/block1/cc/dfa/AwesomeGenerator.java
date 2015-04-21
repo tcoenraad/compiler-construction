a5a6d17a65a90844b53adff989cd90a1e81f1a53
Twan Coenraad <twan@coenraad.at>
2015-04-21 21:05:47 +0200

    Added awesome generator

--- block-1/pp/block1/cc/dfa/AwesomeGenerator.java
+++ block-1/pp/block1/cc/dfa/AwesomeGenerator.java
@@ -1,9 +1,28 @@
 package pp.block1.cc.dfa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AwesomeGenerator implements Generator {
     public List<String> scan(State dfa, String text) {
-        return null;
+        List<String> result = new ArrayList<String>();
+
+        int i = 0;
+        while (i < text.length()) {
+            State state = dfa;
+            String token = "";
+
+            while (i < text.length()) {
+                state = state.getNext(text.charAt(i));
+                if (state != null) {
+                    token += text.charAt(i);
+                } else {
+                    break;
+                }
+                i++;
+            }
+            result.add(token);
+        }
+        return result;
     }
 }
