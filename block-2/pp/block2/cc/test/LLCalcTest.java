6618fa649a7a15f06c86d6e267f6747676dc37f0
Twan Coenraad <twan@coenraad.at>
2015-05-01 12:11:38 +0200

    Restructured test to be far more helpful when debugging

--- block-2/pp/block2/cc/test/LLCalcTest.java
+++ block-2/pp/block2/cc/test/LLCalcTest.java
@@ -24,14 +24,11 @@ public class LLCalcTest {
 	@Test
 	public void testSentence() {
 		Grammar g = Grammars.makeSentence();
-		// Without the last (recursive) rule, the grammar is LL-1
-		assertTrue(createCalc(g).isLL1());
+        LLCalc calc = createCalc(g);
 		NonTerm subj = g.getNonterminal("Subject");
 		NonTerm obj = g.getNonterminal("Object");
 		NonTerm sent = g.getNonterminal("Sentence");
 		NonTerm mod = g.getNonterminal("Modifier");
-		g.addRule(mod, mod, mod);
-		LLCalc calc = createCalc(g);
 		// FIRST sets
 		Term adj = g.getTerminal(Sentence.ADJECTIVE);
 		Term noun = g.getTerminal(Sentence.NOUN);
@@ -47,7 +44,11 @@ public class LLCalcTest {
 		assertEquals(set(end), calc.getFollow().get(obj));
 		assertEquals(set(noun, adj), calc.getFollow().get(mod));
 		// is-LL1-test
-		assertFalse(calc.isLL1());
+        assertTrue(calc.isLL1());
+        // Without this (recursive) rule, the grammar is LL-1
+        g.addRule(mod, mod, mod);
+        calc = createCalc(g);
+        assertFalse(calc.isLL1());
 	}
 
 	/** Creates an LL1-calculator for a given grammar. */
