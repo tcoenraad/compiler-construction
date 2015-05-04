0b6613683756dd0d6ad989dafef1a9d5e017d4f0
Twan Coenraad <twan@coenraad.at>
2015-05-04 20:20:55 +0200

    Refactored code to be far more readable and recursive

--- block-2/pp/block2/cc/ll/GenericLLParser.java
+++ block-2/pp/block2/cc/ll/GenericLLParser.java
@@ -4,20 +4,113 @@ import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.Token;
 import pp.block2.cc.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /** Generic table-driven LL(1)-parser. */
 public class GenericLLParser implements Parser {
+	public GenericLLParser(Grammar g) {
+		this.g = g;
+		this.calc = new AwesomeLLCalc(g);
+	}
 
 	private final Grammar g;
 	private final LLCalc calc;
 
-	public GenericLLParser(Grammar g) {
-		this.g = g;
-		this.calc = new AwesomeLLCalc(g);
-		getLL1Table();
+    private int index;
+    private List<? extends Token> tokens;
+
+	@Override
+	public AST parse(Lexer lexer) throws ParseException {
+		this.tokens = lexer.getAllTokens();
+		this.index = 0;
+		return parse(g.getStart());
 	}
-    
+
+	/** Parses the start of the token stream according to a given
+	 * symbol. If it is a terminal, that should be the first token;
+	 * otherwise, it is a non-terminal that should be expanded 
+	 * according to the next token in the token stream, using the
+	 * FIRST+-lookup table and recursively calling {@link #parse(Rule)}
+	 * @param symb the symbol according to which the token stream
+	 * should be parsed
+	 * @return the sub-AST resulting from the parsing of symb;
+	 * or null if the symbol expands to the empty string
+	 * @throws ParseException if the symbol cannot be parsed
+	 * because the token stream does not contain the expected symbols
+	 */
+	private AST parse(Symbol symb) throws ParseException {
+		if (symb instanceof Term) {
+            if (((Term) symb).getTokenType() != peek().getType()) {
+                throw new ParseException("Could not parse Term");
+            }
+            return new AST((Term) symb, tokens.get(next().getType()));
+        } else {
+            for (Map.Entry<Rule, Set<Term>> entry : calc.getFirstp().entrySet()) {
+                if (!entry.getKey().getLHS().equals(symb) && !entry.getValue().contains(peek())) {
+                    continue;
+                }
+                return parse(entry.getKey());
+            }
+            throw new ParseException("Could not parse NonTerm");
+        }
+	}
+
+	/** Parses the start of the token stream according to a given
+	 * rule, recursively calling {@link #parse(Symbol)} to process
+	 * the RHS.
+	 * @return the sub-AST resulting from the parsing of the rule.
+	 * The top node is the node for the LHS of the rule, its direct
+	 * children correspond to the symbols of the rule's RHS.
+	 * @throws ParseException if the symbol cannot be parsed
+	 * because the token stream does not contain the expected symbols
+	 */
+	private AST parse(Rule rule) throws ParseException {
+        AST ast = new AST(rule.getLHS());
+		for (Symbol symbol : rule.getRHS()) {
+            ast.addChild(parse(symbol));
+        }
+        return ast;
+	}
+
+	/** Returns the next token, without moving the token index. */
+	private Token peek() throws ParseException {
+		if (index >= tokens.size()) {
+			throw new ParseException("Reading beyond end of input");
+		}
+		return tokens.get(index);
+	}
+
+	/** Returns the next token and moves up the token index. */
+	private Token next() throws ParseException {
+		Token result = peek();
+		index++;
+		return result;
+	}
+
+	/** Uses the lookup table to look up the rule to which
+	 * a given nonterminal should be expanded.
+	 * The next rule is determined by the next token, using the
+	 * FIRST+-set of the nonterminal.
+	 * @throws ParseException if the lookup table does not 
+	 * contain a rule for the nonterminal in combination with
+	 * the first token in the token stream.
+	 */
+	private Rule lookup(NonTerm nt) throws ParseException {
+		Token next = peek();
+		Rule result = getLL1Table().get(nt).get(next.getType());
+		if (result == null) {
+			throw new ParseException(String.format(
+					"Line %d:%d - no rule for '%s' on token '%s'",
+					next.getLine(), next.getCharPositionInLine(),
+					nt.getName(), next));
+		}
+		return result;
+	}
+
+	/** Lazily builds and then returns the lookup table. */
 	private Map<NonTerm, List<Rule>> getLL1Table() {
 		if (ll1Table == null) {
 			ll1Table = calcLL1Table();
@@ -27,130 +120,13 @@ public class GenericLLParser implements Parser {
 
 	/** Constructs the {@link #ll1Table}. */
 	private Map<NonTerm, List<Rule>> calcLL1Table() {
-		ll1Table = new HashMap<>();
-		Map<Rule, Set<Term>> firstp = calc.getFirstp();
-
-		for (NonTerm nt : g.getNonterminals()) {
-			for (int i = 0; i < g.getTerminals().size(); i++) {
-                ArrayList<Rule> list = new ArrayList<>();
-				ll1Table.put(nt, list);
-                for (int j = 0; j < g.getTerminals().size() + 1; j++) {
-                    list.add(j, null);
-                }
-			}
-
-            for (Rule p : g.getRules(nt)) {
-                for (Symbol w : firstp.get(p)) {
-                    if (w instanceof Term) {
-                        List<Rule> l = ll1Table.get(p.getLHS());
-                        l.set(((Term) w).getTokenType(), p);
-                    }
-
-                }
-                if (p.getRHS().contains(Symbol.EOF)) {
-                    List<Rule> l = ll1Table.get(p.getLHS());
-                    l.set(l.size() - 1, p);
-                }
-            }
-		}
-		return ll1Table;
-	}
+        Map<NonTerm, List<Rule>> ll1Table = new HashMap<>();
+        for (NonTerm nonTerm : g.getNonterminals()) {
+            ll1Table.put(nonTerm, g.getRules(nonTerm));
+        }
+        return ll1Table;
+    }
 
 	/** Map from non-terminals to lists of rules indexed by token type. */
 	private Map<NonTerm, List<Rule>> ll1Table;
-
-	@Override
-	public AST parse(Lexer lexer) throws ParseException {
-		AST tree = null;
-		AST currentBranch = null;
-		Token word = lexer.nextToken();
-		Stack<Symbol> stack = new Stack<>();
-		stack.push(Symbol.EOF);
-		stack.push(g.getStart());
-		Symbol focus = stack.lastElement();
-		while (true){
-			if (focus.equals(Symbol.EOF) && word.getText().equals("<EOF>")){
-				return tree;
-			} else if(focus instanceof Term || focus.equals(Symbol.EOF)) {
-				if (((Term) focus).getTokenType() == word.getType() || word.equals("<EOF>")) {
-					Symbol s = stack.pop();
-					currentBranch.addChild(new AST((Term) s, word)); // adding branch
-					word = lexer.nextToken();
-					currentBranch = goUp(currentBranch, tree);
-				} else if (focus.equals(Symbol.EMPTY)){
-					stack.pop();
-					currentBranch = goUp(currentBranch, tree);
-				} else {
-					throw new ParseException("Error while looking for symbol at top of stack");
-				}
-			} else {
-				if (ll1Table.get(focus).get(word.getType()) != null){
-					List<Symbol> l = ll1Table.get(focus).get(word.getType()).getRHS();
-					Symbol s = stack.pop();
-					if (tree == null){
-						tree = new AST((NonTerm) s);
-						currentBranch = tree;
-					} else {
-						int childrenLength = currentBranch.getChildren().size();
-						currentBranch.addChild(new AST((NonTerm) s)); // adding branch
-						currentBranch = currentBranch.getChildren().get(childrenLength); // switching branch
-					}
-					for (int i = l.size() - 1; i >= 0; i--) {
-						stack.add(l.get(i));
-					}
-				} else{
-					throw new ParseException("Error while expanding focus");
-				}
-			}
-			focus = stack.lastElement();
-
-		}
-	}
-
-	private AST goUp(AST currentBranch, AST tree) {
-		if(currentBranch.equals(tree)){
-			return currentBranch;
-		} else if (currentBranch.isTerminal()){
-			return goUp(goOneUp(currentBranch, tree), tree);
-		} else if (finishedRule(currentBranch)){
-			return goUp(goOneUp(currentBranch, tree), tree);
-		} else{
-			return currentBranch;
-		}
-	}
-
-	private boolean finishedRule(AST currentBranch) {
-		boolean finishedRule = false;
-		if (currentBranch.isTerminal() || currentBranch.getSymbol().equals(Symbol.EMPTY)){
-			finishedRule = true;
-		} else {
-			List<Rule> rules = g.getRules((NonTerm) currentBranch.getSymbol());
-			for (int i = 0; !finishedRule && i < rules.size(); i++) {
-                Rule rule = rules.get(i);
-				if (currentBranch.getChildren().size() != rule.getRHS().size()) {
-                    continue;
-                }
-                for (int j = 0; j < currentBranch.getChildren().size(); j++) {
-                    finishedRule = currentBranch.getChildren().get(j).getSymbol().equals(rule.getRHS().get(j)) && // symbol on current branch and rule match
-                            (currentBranch.getChildren().get(j).isTerminal() ||
-                                    finishedRule(currentBranch.getChildren().get(j)));
-                }
-			}
-		}
-		return finishedRule;
-	}
-
-	private AST goOneUp(AST currentBranch, AST tree){
-		if (tree.equals(currentBranch)){
-			return tree;
-		}
-        for(int i = tree.getChildren().size() - 1; i >= 0; i--) {
-            if (tree.getChildren().get(i).equals(currentBranch)){
-                return tree;
-            } else {
-                return goOneUp(currentBranch, tree.getChildren().get(i));
-            }
-        }
-		return null;
-	}
 }
