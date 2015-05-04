b83efccb7d717845c4e95903aa7999fa3b5bd9dd
Twan Coenraad <twan@coenraad.at>
2015-05-04 14:09:27 +0200

    Refactored CC7

--- block-2/pp/block2/cc/ll/CC4Parser.java
+++ block-2/pp/block2/cc/ll/CC4Parser.java
@@ -11,125 +11,123 @@ import java.util.List;
 import static pp.block2.cc.ll.CC4.*;
 
 
-/**
- * Created by Jip on 2-5-2015.
- */
 public class CC4Parser implements Parser {
     private static final NonTerm L = new NonTerm("L");
     private static final NonTerm R = new NonTerm("R");
     private static final NonTerm Q = new NonTerm("Q");
     private static final NonTerm R2 = new NonTerm("R2");
     private static final NonTerm Q2 = new NonTerm("Q2");
+
+    private final SymbolFactory fact;
+    private List<? extends Token> tokens;
+    private int index;
+
     public CC4Parser() {
         this.fact = new SymbolFactory(CC4.class);
     }
-
-    private final SymbolFactory fact;
 
     @Override
     public AST parse(Lexer lexer) throws ParseException {
         this.tokens = lexer.getAllTokens();
         this.index = 0;
-        return parseL();
+        return parseL(); // L == start symbol
     }
 
-    private List<? extends Token> tokens;
 
     private AST parseL() throws ParseException {
-        AST result = new AST(L);
-        // there is only one rule, no need to look at token
+        AST tree = new AST(L);
         Token next = peek();
         switch(next.getType()){
             case A:
-                result.addChild(parseR());
-                result.addChild(parseToken(A));
+                tree.addChild(parseR());
+                tree.addChild(parseToken(A));
                 break;
             case B:
-                result.addChild(parseQ());
-                result.addChild(parseToken(B));
-                result.addChild(parseToken(A));
+                tree.addChild(parseQ());
+                tree.addChild(parseToken(B));
+                tree.addChild(parseToken(A));
                 break;
             case C:
-                result.addChild(parseR());
-                result.addChild(parseToken(A));
+                tree.addChild(parseR());
+                tree.addChild(parseToken(A));
                 break;
             default:
-                System.out.println("Mijn type is: " + next.getType());
                 throw unparsable(L);
-
         }
-        return result;
+        return tree;
     }
 
     private AST parseR() throws ParseException {
-        AST result = new AST(R);
+        AST tree = new AST(R);
         Token next = peek();
         switch (next.getType()) {
             case A:
-                result.addChild(parseToken(A));
-                result.addChild(parseToken(B));
-                result.addChild(parseToken(A));
-                result.addChild(parseR2());
+                tree.addChild(parseToken(A));
+                tree.addChild(parseToken(B));
+                tree.addChild(parseToken(A));
+                tree.addChild(parseR2());
                 break;
             case C:
-                result.addChild(parseToken(C));
-                result.addChild(parseToken(A));
-                result.addChild(parseToken(B));
-                result.addChild(parseToken(A));
-                result.addChild(parseR2());
+                tree.addChild(parseToken(C));
+                tree.addChild(parseToken(A));
+                tree.addChild(parseToken(B));
+                tree.addChild(parseToken(A));
+                tree.addChild(parseR2());
                 break;
             default:
                 throw unparsable(R);
 
         }
-        return result;
+        return tree;
     }
 
     private AST parseQ() throws ParseException {
-        AST result = new AST(Q);
-        // choose between rules based on the lookahead
-        result.addChild(parseToken(B));
-        result.addChild(parseQ2());
-        return result;
+        AST tree = new AST(Q);
+        Token next = peek();
+        switch (next.getType()) {
+            case B:
+                tree.addChild(parseToken(B));
+                tree.addChild(parseQ2());
+                break;
+            default:
+                throw unparsable(Q);
+        }
+        return tree;
     }
 
 
     private AST parseR2() throws ParseException {
-        AST result = new AST(R2);
+        AST tree = new AST(R2);
         Token next = peek();
-        if(next != null){
-            System.out.println(next.getType());
-            switch (next.getType()){
-                case B:
-                    result.addChild(parseToken(B));
-                    result.addChild(parseToken(C));
-                    result.addChild(parseR2());
-                    break;
-                case A:
-                    break;
-                default:
-                    throw unparsable(R2);
-            }
+        switch (next.getType()) {
+            case B:
+                tree.addChild(parseToken(B));
+                tree.addChild(parseToken(C));
+                tree.addChild(parseR2());
+                break;
+            case A:
+                break;
+            default:
+                throw unparsable(R2);
         }
-
-        return result;
+        return tree;
     }
 
     private AST parseQ2() throws ParseException {
-        AST result = new AST(Q2);
+        AST tree = new AST(Q2);
         Token next = peek();
         switch(next.getType()){
             case B:
-                result.addChild(parseToken(B));
-                result.addChild(parseToken(C));
+                tree.addChild(parseToken(B));
+                tree.addChild(parseToken(C));
                 break;
             case C:
-                result.addChild(parseToken(C));
+                tree.addChild(parseToken(C));
                 break;
             default:
                 throw unparsable(Q2);
         }
-        return result;
+        return tree;
     }
 
     private ParseException unparsable(NonTerm nt) {
@@ -172,15 +170,11 @@ public class CC4Parser implements Parser {
         return result;
     }
 
-    private int index;
-
-
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("Usage: [text]+");
         } else {
             for (String text : args) {
-                System.out.println("Tekst is: " + text);
                 CharStream stream = new ANTLRInputStream(text);
                 Lexer lexer = new CC4(stream);
                 try {
