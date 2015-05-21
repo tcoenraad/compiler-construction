2d70eb6fa240d96369eeb9ef1fad4ad26bace823
Jip Spel <jipspel@gmail.com>
2015-05-21 17:12:37 +0200

    Added a compiler for grammar Calc.g4

--- block-4/pp/block4/cc/iloc/CalcCompiler.java
+++ block-4/pp/block4/cc/iloc/CalcCompiler.java
@@ -1,22 +1,19 @@
 package pp.block4.cc.iloc;
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTree;
-
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import pp.block4.cc.ErrorListener;
 import pp.iloc.Simulator;
-import pp.iloc.model.Op;
-import pp.iloc.model.OpCode;
-import pp.iloc.model.Operand;
-import pp.iloc.model.Program;
+import pp.iloc.model.*;
 
 public class CalcCompiler extends CalcBaseListener {
 	/** Program under construction. */
 	private Program prog;
+	private ParseTreeProperty<Reg> registers;
+    private int numberOfRegisters;
 	// Attribute maps and other fields
 
 	/** Compiles a given expression string into an ILOC program. */
@@ -45,26 +42,75 @@ public class CalcCompiler extends CalcBaseListener {
 
 	/** Compiles a given Calc-parse tree into an ILOC program. */
 	public Program compile(ParseTree tree) {
-		// Fill in
+        prog = new Program();
+		registers = new ParseTreeProperty<>();
+        numberOfRegisters = 0;
+
+        addNewRegister(tree);
+        emit(OpCode.loadI, new Num(0), registers.get(tree));
+        new ParseTreeWalker().walk(this, tree);
+        emit(OpCode.out, new Str(""), new Reg("r_" + (numberOfRegisters-1)));
+        return prog;
 	}
 
 	/** Constructs an operation from the parameters 
 	 * and adds it to the program under construction. */
 	private void emit(OpCode opCode, Operand... args) {
-		this.prog.addInstr(new Op(opCode, args));
-	}
+        prog.addInstr(new Op(opCode, args));
+    }
 
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.err.println("Usage: [expr]+");
-			return;
-		}
-		CalcCompiler compiler = new CalcCompiler();
-		for (String expr : args) {
-			System.out.println("Processing " + expr);
-			Program prog = compiler.compile(expr);
-			new Simulator(prog).run();
-			System.out.println(prog.prettyPrint());
-		}
-	}
+    /** Adds a new register to the list of registers. */
+    private void addNewRegister(ParseTree node){
+        registers.put(node, new Reg("r_" + numberOfRegisters));
+        numberOfRegisters++;
+    }
+
+	@Override
+    public void exitPar(@NotNull CalcParser.ParContext ctx) {
+        addNewRegister(ctx);
+        emit(OpCode.i2i, registers.get(ctx.expr()), registers.get(ctx));
+    }
+
+    @Override
+    public void exitMinus(@NotNull CalcParser.MinusContext ctx) {
+        Reg expr = registers.get(ctx.expr());
+        addNewRegister(ctx);
+        emit(OpCode.sub, new Reg("r_0"), expr, registers.get(ctx));
+    }
+
+    @Override
+    public void exitNumber(@NotNull CalcParser.NumberContext ctx) {
+        addNewRegister(ctx);
+        emit(OpCode.loadI, new Num(Integer.parseInt(ctx.getText())), registers.get(ctx));
+    }
+
+    @Override
+    public void exitTimes(@NotNull CalcParser.TimesContext ctx) {
+        Reg expr0 = registers.get(ctx.expr(0));
+        Reg expr1 = registers.get(ctx.expr(1));
+        addNewRegister(ctx);
+        emit(OpCode.mult, expr0, expr1, registers.get(ctx));
+    }
+
+    @Override
+    public void exitPlus(@NotNull CalcParser.PlusContext ctx) {
+        Reg expr0 = registers.get(ctx.expr(0));
+        Reg expr1 = registers.get(ctx.expr(1));
+        addNewRegister(ctx);
+        emit(OpCode.add, expr0, expr1, registers.get(ctx));
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Usage: [expr]+");
+            return;
+        }
+        CalcCompiler compiler = new CalcCompiler();
+        for (String expr : args) {
+            System.out.println("Processing " + expr);
+            Program prog = compiler.compile(expr);
+            new Simulator(prog).run();
+            System.out.println(prog.prettyPrint());
+        }
+    }
 }
