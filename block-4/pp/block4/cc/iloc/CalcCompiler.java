0f1b6fc3c73260a4148ed98578e2fd19df5e0625
Twan Coenraad <twan@coenraad.at>
2015-05-24 16:04:59 +0200

    Converted tabs to spaces for CalcCompiler as well

--- block-4/pp/block4/cc/iloc/CalcCompiler.java
+++ block-4/pp/block4/cc/iloc/CalcCompiler.java
@@ -11,52 +11,52 @@ import pp.iloc.model.*;
 
 /** Compiler from Calc.g4 to ILOC. */
 public class CalcCompiler extends CalcBaseListener {
-	/** Program under construction. */
-	private Program prog;
+    /** Program under construction. */
+    private Program prog;
 
     // Attribute maps and other fields
-	private ParseTreeProperty<Reg> registers;
+    private ParseTreeProperty<Reg> registers;
     private int numberOfRegisters;
 
-	/** Compiles a given expression string into an ILOC program. */
-	public Program compile(String text) {
-		Program result = null;
-		ErrorListener listener = new ErrorListener();
-		CharStream chars = new ANTLRInputStream(text);
-		Lexer lexer = new CalcLexer(chars);
-		lexer.removeErrorListeners();
-		lexer.addErrorListener(listener);
-		TokenStream tokens = new CommonTokenStream(lexer);
-		CalcParser parser = new CalcParser(tokens);
-		parser.removeErrorListeners();
-		parser.addErrorListener(listener);
-		ParseTree tree = parser.complete();
-		if (listener.hasErrors()) {
-			System.out.printf("Parse errors in %s:%n", text);
-			for (String error : listener.getErrors()) {
-				System.err.println(error);
-			}
-		} else {
-			result = compile(tree);
-		}
-		return result;
-	}
+    /** Compiles a given expression string into an ILOC program. */
+    public Program compile(String text) {
+        Program result = null;
+        ErrorListener listener = new ErrorListener();
+        CharStream chars = new ANTLRInputStream(text);
+        Lexer lexer = new CalcLexer(chars);
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(listener);
+        TokenStream tokens = new CommonTokenStream(lexer);
+        CalcParser parser = new CalcParser(tokens);
+        parser.removeErrorListeners();
+        parser.addErrorListener(listener);
+        ParseTree tree = parser.complete();
+        if (listener.hasErrors()) {
+            System.out.printf("Parse errors in %s:%n", text);
+            for (String error : listener.getErrors()) {
+                System.err.println(error);
+            }
+        } else {
+            result = compile(tree);
+        }
+        return result;
+    }
 
-	/** Compiles a given Calc-parse tree into an ILOC program. */
-	public Program compile(ParseTree tree) {
+    /** Compiles a given Calc-parse tree into an ILOC program. */
+    public Program compile(ParseTree tree) {
         prog = new Program();
-		registers = new ParseTreeProperty<>();
+        registers = new ParseTreeProperty<>();
         numberOfRegisters = 0;
 
         addNewRegister(tree);
         new ParseTreeWalker().walk(this, tree);
         emit(OpCode.out, new Str(""), new Reg("r_" + (numberOfRegisters - 1)));
         return prog;
-	}
+    }
 
-	/** Constructs an operation from the parameters 
-	 * and adds it to the program under construction. */
-	private void emit(OpCode opCode, Operand... args) {
+    /** Constructs an operation from the parameters
+     * and adds it to the program under construction. */
+    private void emit(OpCode opCode, Operand... args) {
         prog.addInstr(new Op(opCode, args));
     }
 
@@ -66,7 +66,7 @@ public class CalcCompiler extends CalcBaseListener {
         numberOfRegisters++;
     }
 
-	@Override
+    @Override
     public void exitPar(@NotNull CalcParser.ParContext ctx) {
         addNewRegister(ctx);
         emit(OpCode.i2i, registers.get(ctx.expr()), registers.get(ctx));
@@ -104,18 +104,18 @@ public class CalcCompiler extends CalcBaseListener {
         emit(OpCode.add, expr0, expr1, registers.get(ctx));
     }
 
-	/** Calls the compiler, and simulates and prints the compiled program. */
-	public static void main(String[] args) {
-		if (args.length == 0) {
-			System.err.println("Usage: [expr]+");
-			return;
-		}
-		CalcCompiler compiler = new CalcCompiler();
-		for (String expr : args) {
-			System.out.println("Processing " + expr);
-			Program prog = compiler.compile(expr);
-			new Simulator(prog).run();
-			System.out.println(prog.prettyPrint());
-		}
-	}
+    /** Calls the compiler, and simulates and prints the compiled program. */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Usage: [expr]+");
+            return;
+        }
+        CalcCompiler compiler = new CalcCompiler();
+        for (String expr : args) {
+            System.out.println("Processing " + expr);
+            Program prog = compiler.compile(expr);
+            new Simulator(prog).run();
+            System.out.println(prog.prettyPrint());
+        }
+    }
 }
