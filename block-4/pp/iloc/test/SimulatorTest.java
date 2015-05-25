c5240bfa45fbb0e18df8b5ff479d3a214abaeb35
Twan Coenraad <twan@coenraad.at>
2015-05-25 23:11:21 +0200

    Revert "Removed tests for files that are not in the Blackboard package"
    As missing file is included per ILOC v5
    
    This reverts commit 9c845b8d5e7c7de25ffb7b46be79d53e64362f5c.

--- block-4/pp/iloc/test/SimulatorTest.java
+++ block-4/pp/iloc/test/SimulatorTest.java
@@ -45,6 +45,23 @@ public class SimulatorTest {
 		assertEquals(240, c.load(a));
 	}
 
+	@Test
+	//(timeout = 1000)
+	public void testFig13Init() {
+		Program p = parse("fig1-3-init");
+		Machine c = new Machine();
+		c.store(2, p.getSymb("a"));
+		c.store(3, p.getSymb("b"));
+		c.store(4, p.getSymb("c"));
+		c.store(5, p.getSymb("d"));
+		new Simulator(p, c).run();
+		if (SHOW) {
+			System.out.println(c);
+			System.out.println(p.prettyPrint());
+		}
+		assertEquals(240, c.load(p.getSymb("a")));
+	}
+
 	Program parse(String filename) {
 		File file = new File(filename + ".iloc");
 		if (!file.exists()) {
