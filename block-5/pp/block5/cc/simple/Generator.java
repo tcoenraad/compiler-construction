9d9dcd817090c053f29c37215e71b0c5cc346c20
Twan Coenraad <twan@coenraad.at>
2015-06-01 10:51:53 +0200

    Converted tabs into spaces for Generator

--- block-5/pp/block5/cc/simple/Generator.java
+++ block-5/pp/block5/cc/simple/Generator.java
@@ -12,123 +12,123 @@ import pp.iloc.Simulator;
 import pp.iloc.model.*;
 /** Class to generate ILOC code for Simple Pascal. */
 public class Generator extends SimplePascalBaseVisitor<Op> {
-	/**
-	 * The representation of the boolean value <code>false</code>.
-	 */
-	public final static Num FALSE_VALUE = new Num(Simulator.FALSE);
-	/**
-	 * The representation of the boolean value <code>true</code>.
-	 */
-	public final static Num TRUE_VALUE = new Num(Simulator.TRUE);
+    /**
+     * The representation of the boolean value <code>false</code>.
+     */
+    public final static Num FALSE_VALUE = new Num(Simulator.FALSE);
+    /**
+     * The representation of the boolean value <code>true</code>.
+     */
+    public final static Num TRUE_VALUE = new Num(Simulator.TRUE);
 
-	/**
-	 * The base register.
-	 */
-	private Reg arp = new Reg("r_arp");
-	/**
-	 * The outcome of the checker phase.
-	 */
-	private Result checkResult;
-	/**
-	 * Association of statement nodes to labels.
-	 */
-	private ParseTreeProperty<Label> labels;
-	/**
-	 * The program being built.
-	 */
-	private Program program;
-	/**
-	 * Register count, used to generate fresh registers.
-	 */
-	private int regCount;
-	/**
-	 * Association of expression and target nodes to registers.
-	 */
-	private ParseTreeProperty<Reg> regs;
+    /**
+     * The base register.
+     */
+    private Reg arp = new Reg("r_arp");
+    /**
+     * The outcome of the checker phase.
+     */
+    private Result checkResult;
+    /**
+     * Association of statement nodes to labels.
+     */
+    private ParseTreeProperty<Label> labels;
+    /**
+     * The program being built.
+     */
+    private Program program;
+    /**
+     * Register count, used to generate fresh registers.
+     */
+    private int regCount;
+    /**
+     * Association of expression and target nodes to registers.
+     */
+    private ParseTreeProperty<Reg> regs;
 
-	public Program generate(ParseTree tree, Result checkResult) {
-		this.program = new Program();
-		this.checkResult = checkResult;
-		this.regs = new ParseTreeProperty<>();
-		this.labels = new ParseTreeProperty<>();
-		this.regCount = 0;
-		tree.accept(this);
-		return this.program;
-	}
+    public Program generate(ParseTree tree, Result checkResult) {
+        this.program = new Program();
+        this.checkResult = checkResult;
+        this.regs = new ParseTreeProperty<>();
+        this.labels = new ParseTreeProperty<>();
+        this.regCount = 0;
+        tree.accept(this);
+        return this.program;
+    }
 
-	/**
-	 * Constructs an operation from the parameters
-	 * and adds it to the program under construction.
-	 */
-	private Op emit(Label label, OpCode opCode, Operand... args) {
-		Op result = new Op(label, opCode, args);
-		this.program.addInstr(result);
-		return result;
-	}
+    /**
+     * Constructs an operation from the parameters
+     * and adds it to the program under construction.
+     */
+    private Op emit(Label label, OpCode opCode, Operand... args) {
+        Op result = new Op(label, opCode, args);
+        this.program.addInstr(result);
+        return result;
+    }
 
-	/**
-	 * Constructs an operation from the parameters
-	 * and adds it to the program under construction.
-	 */
-	private Op emit(OpCode opCode, Operand... args) {
-		return emit((Label) null, opCode, args);
-	}
+    /**
+     * Constructs an operation from the parameters
+     * and adds it to the program under construction.
+     */
+    private Op emit(OpCode opCode, Operand... args) {
+        return emit((Label) null, opCode, args);
+    }
 
-	/**
-	 * Looks up the label for a given parse tree node,
-	 * creating it if none has been created before.
-	 * The label is actually constructed from the entry node
-	 * in the flow graph, as stored in the checker result.
-	 */
-	private Label getOrCreateLabel(ParserRuleContext node) {
-		Label result = this.labels.get(node);
-		if (result == null) {
-			ParserRuleContext entry = this.checkResult.getEntry(node);
-			result = createLabel(entry, "n");
-			this.labels.put(node, result);
-		}
-		return result;
-	}
+    /**
+     * Looks up the label for a given parse tree node,
+     * creating it if none has been created before.
+     * The label is actually constructed from the entry node
+     * in the flow graph, as stored in the checker result.
+     */
+    private Label getOrCreateLabel(ParserRuleContext node) {
+        Label result = this.labels.get(node);
+        if (result == null) {
+            ParserRuleContext entry = this.checkResult.getEntry(node);
+            result = createLabel(entry, "n");
+            this.labels.put(node, result);
+        }
+        return result;
+    }
 
-	/**
-	 * Creates a label for a given parse tree node and prefix.
-	 */
-	private Label createLabel(ParserRuleContext node, String prefix) {
-		Token token = node.getStart();
-		int line = token.getLine();
-		int column = token.getCharPositionInLine();
-		String result = prefix + "_" + line + "_" + column;
-		return new Label(result);
-	}
+    /**
+     * Creates a label for a given parse tree node and prefix.
+     */
+    private Label createLabel(ParserRuleContext node, String prefix) {
+        Token token = node.getStart();
+        int line = token.getLine();
+        int column = token.getCharPositionInLine();
+        String result = prefix + "_" + line + "_" + column;
+        return new Label(result);
+    }
 
-	/**
-	 * Retrieves the offset of a variable node from the checker result,
-	 * wrapped in a {@link Num} operand.
-	 */
-	private Num getOffset(ParseTree node) {
-		return new Num(this.checkResult.getOffset(node));
-	}
+    /**
+     * Retrieves the offset of a variable node from the checker result,
+     * wrapped in a {@link Num} operand.
+     */
+    private Num getOffset(ParseTree node) {
+        return new Num(this.checkResult.getOffset(node));
+    }
 
-	/**
-	 * Returns a register for a given parse tree node,
-	 * creating a fresh register if there is none for that node.
-	 */
-	private Reg getOrCreateReg(ParseTree node) {
-		Reg result = this.regs.get(node);
-		if (result == null) {
+    /**
+     * Returns a register for a given parse tree node,
+     * creating a fresh register if there is none for that node.
+     */
+    private Reg getOrCreateReg(ParseTree node) {
+        Reg result = this.regs.get(node);
+        if (result == null) {
             result = new Reg("r_" + this.regCount);
-			this.regs.put(node, result);
+            this.regs.put(node, result);
             this.regCount++;
-		}
-		return result;
-	}
+        }
+        return result;
+    }
 
-	/**
-	 * Assigns a register to a given parse tree node.
-	 */
-	private void setReg(ParseTree node, Reg reg) {
-		this.regs.put(node, reg);
-	}
+    /**
+     * Assigns a register to a given parse tree node.
+     */
+    private void setReg(ParseTree node, Reg reg) {
+        this.regs.put(node, reg);
+    }
 
     @Override
     public Op visitProgram(@NotNull SimplePascalParser.ProgramContext ctx) {
